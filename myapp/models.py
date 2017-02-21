e14ea32e3f4cd4f971c7be63813ca5e29f04be0b
carlosmrl505@gmail.com <carlosrodriguez@CarlosMBA.local>
2017-02-21 14:36:01 -0500

    Linter addition to Atom

--- myapp/models.py
+++ myapp/models.py
@@ -3,16 +3,21 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Post (models.Model):
+
+class Post(models.Model):
+    """Class describing a user's post."""
+
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    text= models.TextField()
-    created_date= models.DateTimeField(default=timezone.now)
-    published_date= models.DateTimeField(blank=True, null=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date= timezone.now()
+        """Method to publis a post."""
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
+        """Return post title."""
         return self.title
