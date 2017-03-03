4f76fb1e285baca0482dcf6695bb5bd169e42d38
carlosmrl505@gmail.com <carlosrodriguez@CarlosMBA.local>
2017-03-02 19:30:29 -0500

    Added security

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -16,6 +17,7 @@ def post_detail(request, pk):
     return render(request, 'myapp/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -30,6 +32,7 @@ def post_new(request):
     return render(request, 'myapp/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -45,12 +48,14 @@ def post_edit(request, pk):
     return render(request, 'myapp/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
     posts = (Post.objects.filter(published_date__isnull=True)
              .order_by('created_date'))
     return render(request, 'myapp/post_draft_list.html', {'posts': posts})
 
 
+@login_required
 def post_publish(request, pk):
     # Note that myapp is not used on this redirect from a different url
     post = get_object_or_404(Post, pk=pk)
@@ -58,6 +63,7 @@ def post_publish(request, pk):
     return redirect('post_detail', post.pk)
 
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
